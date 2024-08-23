59be33b7e6b83ca4eaff2d8dbf4f6f1607ac3297
Zach Lloyd <zachlloyd@Zachs-MBP.attlocal.net>
2024-08-23 14:12:00 -0400

    added function to display the currently selected project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 import { displayProjects, createProject } from "./project-dom.js";
+import { displaySelectedProject } from "./todo-dom.js";
 
 createProject();
 displayProjects();
+displaySelectedProject();
--- src/todo-dom.js
+++ src/todo-dom.js
@@ -1,8 +1,37 @@
 import { Project } from "./project.js";
 import { defaultProject } from "./project-dom.js";
 
-const selectedProject = defaultProject;
+const createTextElement = function(elementType, content, parent) {
+    const elt = document.createElement(elementType);
+    elt.textContent = content;
+    parent.appendChild(elt);
+}
 
-const displaySelectedProject = function(proj) {
+export const selectedProject = defaultProject;
+
+export const displaySelectedProject = function() {
+    const currentContainer = document.querySelector(".current-project");
+    const projNameContainer = document.createElement("h1");
+    projNameContainer.textContent = selectedProject.name;
+    currentContainer.appendChild(projNameContainer);
+
+    const itemList = document.createElement("ul");
+
+    for (let i = 0; i < defaultProject.items.length; i++) {
+        const item = defaultProject.items[i];
+        const itemContainer = document.createElement("li");
+
+        createTextElement("h2", item.title, itemContainer);
+        createTextElement("p", item.description, itemContainer);
+        createTextElement("h3", item.dueDate, itemContainer);
+        createTextElement("h3", item.priority, itemContainer);
+
+        itemList.appendChild(itemContainer);
+    }
+
+    currentContainer.appendChild(itemList);
+}
+
+export const changeSelectedProject = function() {
     
 }

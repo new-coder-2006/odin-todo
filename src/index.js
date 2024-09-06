64469ec0d6e40333945c0bef3bde22fa1498d9eb
Zach Lloyd <joseph.z.lloyd@gmail.com>
2024-09-06 15:46:59 -0400

    added comments to todo-dom.js and index.js. Reviewed template.html and confirmed no comments necessary

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import "./styles.css";
 import { displayProjects, createProject, displaySelectedProject, 
     createSelectButtons } from "./project-dom.js";
 import { addTodoItem } from "./todo-dom.js";
-
+// Initial calls to display the user's list of projects and currently selected
+// project and add relevant event listeners
 createProject();
 addTodoItem();
 displayProjects();
--- src/todo-dom.js
+++ src/todo-dom.js
@@ -1,13 +1,15 @@
-import { Project } from "./project.js";
-import { defaultProject, projects, displayProjects, selectedProject, displaySelectedProject } from "./project-dom.js";
+import { displayProjects, selectedProject, displaySelectedProject } from "./project-dom.js";
 import { Todo } from "./todo.js";
-
+/**
+ * Function that allows the user to add a new todo list item to a project
+ */
 export const addTodoItem = function() {
     const newItemButton = document.querySelector(".add-item");
     const form = document.querySelector(".item-dialog");
     const submitButton = document.querySelector(".submit-item");
     const cancelButton = document.querySelector(".cancel-item");
-
+    // When the button is clicked to create an item, show the user a dialog box
+    // with fields for the relevant information
     newItemButton.addEventListener("click", () => {
         form.showModal();
     });
@@ -22,7 +24,9 @@ export const addTodoItem = function() {
         const dateField = document.querySelector("#date");
         const dateValue = dateField.value;
         let dueDate;
-        
+        // If the user enters a due date, format it to mm-dd-yyyy format; 
+        // otherwise, display text noting that the due date is still to be 
+        // determined
         if (dateValue != "") {
             const [year, month, day] = dateValue.split('-');
             const formattedDate = `${month}-${day}-${year}`;
@@ -32,13 +36,20 @@ export const addTodoItem = function() {
         }  
 
         const priorityField = document.querySelector("#high-priority");
+        // Stores Boolean value representing whether the user ticked the box
+        // to mark the item as high priority
         const priority = priorityField.checked;
-
+        // Enforce requirement that the item must have a title (no other fields
+        // are required)
         if (title === "") {
             alert("Please enter a title for this list item!");
         } else {
             const newItem = new Todo(title, description, dueDate, priority);
+            // Can only add items to the project that is currently selected for
+            // display
             selectedProject.addItem(newItem);
+            // Reset form fields after information is submitted and close the 
+            // form
             titleField.value = "";
             descriptionField.value = "";
             dateField.value = "";
@@ -48,7 +59,7 @@ export const addTodoItem = function() {
             displaySelectedProject();
         }
     });
-
+    // Add event listener so that the user can cancel creation of a new item
     cancelButton.addEventListener("click", () => {
         const nameField = document.querySelector("#name");
         nameField.value = "";
